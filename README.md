f68fe364f9fc530d083137e37b3e558860f26858
tatsuro-m <mtatsu0130@gmail.com>
2019-08-25 15:17:22 +0900

    環境開発終了

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+「本はもっと循環して良い」
+そんな思いで作る書籍を知らない人にgiveできるアプリです。
--- config/application.rb
+++ config/application.rb
@@ -15,17 +15,19 @@ module BookGiveApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
 
-  #rspecファイルを自動生成するための記述
-  config.generators do |g|
-    g.test_framework :rspec,
-                     fixtures: true,
-                     view_specs: false,
-                     helper_specs: false,
-                     routing_specs: false,
-                     controller_specs: true,
-                     request_specs: false
-    g.fixture_replacement :factory_bot, dir: "spec/factories"
+    #rspecファイルを自動生成するための記述
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
+
+
